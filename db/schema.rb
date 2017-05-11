b71112d46aa6b2207799ed2418efd0f7f746d927
Yutaka HARA <yutaka.hara+github@gmail.com>
2017-05-12 02:45:54 +0900

    Create table :categories

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161025123631) do
+ActiveRecord::Schema.define(version: 20170510131118) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
@@ -21,6 +25,7 @@ ActiveRecord::Schema.define(version: 20161025123631) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "permanent",    default: false, null: false
+    t.integer  "category_id"
   end
 
 end
